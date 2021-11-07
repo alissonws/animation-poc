cf246612513417d9b95c914c6761af9cf66a31c9
Alisson Corrêa <alissonwcorrea@gmail.com>
2021-11-07 14:35:05 -0300

    add Hero component with text display functionality

---
+++ components/Hero/index.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="hero">
+    <Blob class="blob" />
+    <div id="text-container" class="text-container">
+      <p
+        v-for="(message, index) in messages"
+        :id="'message-' + index"
+        :key="index"
+        class="message-item"
+      >
+        {{ message }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    scale: {
+      type: Number,
+      default: 0.6,
+    },
+  },
+  data() {
+    return {
+      messages: [],
+      messagesQueue: [],
+      isInitializing: true,
+      isBusy: false,
+    }
+  },
+
+  mounted() {
+    this.init()
+  },
+
+  methods: {
+    init() {
+      const self = this
+      const blobScale = this.scale
+
+      const onComplete = function () {
+        self.$emit('startup-animation-finished')
+        self.isInitializing = false
+        self._checkMessageQueue()
+      }
+      this.$KUTE
+        .fromTo(
+          '.blob',
+          { opacity: 0, scale: 0.1, translateX: 0, rotate: 0 },
+          { opacity: 1, scale: blobScale, translateX: 250, rotate: 360 },
+          { duration: 1500, easing: 'easingCircularInOut', onComplete }
+        )
+        .start()
+    },
+    pushText(message) {
+      if (
+        this.messagesQueue.length === 0 &&
+        this.isBusy === false &&
+        this.isInitializing === false
+      ) {
+        this.isBusy = true
+        this._actuallyPushText(message)
+      } else {
+        this.messagesQueue.push(message)
+      }
+    },
+    clearTexts() {
+      if (
+        this.messagesQueue.length === 0 &&
+        this.isBusy === false &&
+        this.isInitializing === false
+      ) {
+        this._actuallyClearTexts()
+      } else {
+        this.messagesQueue.push('>CLEAR')
+      }
+    },
+    _actuallyPushText(message) {
+      this.messages.push(message)
+      this.$nextTick(function () {
+        const messageTween = this._getDisplayMessageTween(message)
+        messageTween.start()
+      })
+    },
+    _actuallyClearTexts() {
+      const self = this
+      const textContainer = document.getElementsByClassName('message-item')
+
+      const onComplete = function () {
+        self.messages = []
+        self._checkMessageQueue()
+      }
+
+      this.$KUTE
+        .allFromTo(
+          textContainer,
+          { opacity: 1, translateY: 0 },
+          { opacity: 0, translateY: -100 },
+          {
+            duration: 650,
+            easing: 'easingSinusoidalInOut',
+            onComplete,
+          }
+        )
+        .start()
+    },
+    _checkMessageQueue() {
+      if (this.messagesQueue.length > 0) {
+        const oldestMessage = this.messagesQueue[0]
+
+        if (oldestMessage === '>CLEAR') {
+          this._actuallyClearTexts()
+        } else {
+          this._actuallyPushText(oldestMessage)
+        }
+
+        this.messagesQueue.splice(0, 1)
+      } else {
+        this.isBusy = false
+      }
+    },
+    _getDisplayMessageTween() {
+      const NEXT_MESSAGE_OFFSET = 800
+      const FADE_IN_DURATION = 800
+      const FADE_IN_DISTANCE = 100
+
+      const self = this
+
+      const onComplete = function () {
+        setTimeout(function () {
+          self._checkMessageQueue()
+        }, NEXT_MESSAGE_OFFSET)
+      }
+      const messageIndex = this.messages.length - 1
+      const messageElemId = '#message-' + messageIndex
+
+      return this.$KUTE.fromTo(
+        messageElemId,
+        { opacity: 0, translateY: FADE_IN_DISTANCE },
+        { opacity: 1, translateY: 0 },
+        {
+          duration: FADE_IN_DURATION,
+          easing: 'easingSinusoidalInOut',
+          onComplete,
+        }
+      )
+    },
+  },
+}
+</script>
+
+<style scoped>
+.hero {
+  z-index: 10;
+
+  position: relative;
+
+  position: absolute;
+  z-index: 99;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none;
+}
+
+.blob {
+  z-index: 50px;
+}
+
+.text-container {
+  position: absolute;
+  left: 15%;
+  top: 35%;
+  width: 40%;
+  z-index: 999;
+  font-size: 2.5rem;
+}
+
+.message-item {
+  opacity: 0;
+}
+</style>

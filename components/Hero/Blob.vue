0816f30bf4458983a231da11ccce66a703e04ddb
Alisson Corrêa <alissonwcorrea@gmail.com>
2021-11-07 03:54:56 -0300

    add base Blob component

---
+++ components/Hero/Blob.vue
@@ -0,0 +1,179 @@
+<template>
+  <div data-section-hero>
+    <div class="z-30 w-full max-w-screen-[60vw] lg:max-w-screen-[40vw]">
+      <svg
+        id="visual"
+        viewBox="0 0 900 900"
+        width="900"
+        height="900"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+      >
+        <g transform="translate(459.4408273086995 453.85861167141223)">
+          <path
+            id="blob-sprite-1"
+            d="M149.7 -158.6C203.9 -133 264.4 -95.1 273.2 -47.8C281.9 -0.5 238.8 56.2 198.6 104C158.3 151.8 120.9 190.5 75 208.2C29.1 225.8 -25.3 222.4 -75.4 205.1C-125.6 187.8 -171.5 156.6 -213.6 110.4C-255.7 64.1 -294 2.8 -293.2 -60.3C-292.3 -123.3 -252.2 -188.1 -196.8 -213.5C-141.4 -238.8 -70.7 -224.9 -11.5 -211.2C47.8 -197.6 95.6 -184.2 149.7 -158.6"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(432.7000459254755 418.89917647538624)">
+          <path
+            id="blob-sprite-2"
+            d="M181.3 -206.7C233.1 -172.5 272.2 -113.7 273.7 -55.6C275.3 2.6 239.4 60 206.8 122C174.2 184 144.8 250.6 92.1 282.7C39.3 314.8 -36.8 312.4 -101 285.6C-165.3 258.7 -217.8 207.4 -233.6 149.6C-249.4 91.8 -228.6 27.6 -207.2 -26.6C-185.8 -80.9 -164 -125.1 -129.3 -162.3C-94.7 -199.5 -47.4 -229.8 8.7 -240.1C64.7 -250.4 129.4 -240.9 181.3 -206.7"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(478.817576415645 468.1065777241524)">
+          <path
+            id="blob-sprite-3"
+            d="M162.1 -202.5C208 -154.6 241.9 -101.6 243.3 -49.4C244.7 2.7 213.7 54.1 180.3 96.8C147 139.5 111.3 173.6 63.7 203.2C16.1 232.7 -43.4 257.8 -102.1 249.8C-160.7 241.7 -218.5 200.6 -256.7 143.7C-295 86.8 -313.7 14.1 -291.4 -40.8C-269.1 -95.8 -205.9 -132.9 -150.5 -179.1C-95.1 -225.4 -47.6 -280.7 5.2 -286.9C58.1 -293.2 116.1 -250.4 162.1 -202.5"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(440.90973077704734 489.62581589288203)">
+          <path
+            id="blob-sprite-4"
+            d="M188.4 -222.6C243.6 -178.3 287.2 -118.3 301.4 -51.2C315.6 15.9 300.2 90.2 258.2 135.8C216.1 181.4 147.2 198.3 87.6 204.1C28 209.9 -22.2 204.5 -74.8 191.5C-127.4 178.5 -182.4 157.7 -225.7 114.8C-269 71.8 -300.7 6.6 -283.7 -43.2C-266.8 -92.9 -201.2 -127.3 -145.8 -171.5C-90.4 -215.7 -45.2 -269.9 10.7 -282.6C66.6 -295.4 133.3 -266.8 188.4 -222.6"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(424.06537507921234 441.02138108452573)">
+          <path
+            id="blob-sprite-5"
+            d="M185.7 -204.7C241.1 -174.8 286.7 -116.7 284.6 -60.3C282.5 -3.9 232.6 50.8 189.5 94.4C146.4 138 110.1 170.5 61.7 202.2C13.3 234 -47.2 265 -96.2 252.3C-145.1 239.7 -182.6 183.3 -205.1 126.9C-227.7 70.4 -235.5 13.7 -232 -47C-228.6 -107.8 -213.9 -172.6 -173.4 -205.1C-132.8 -237.6 -66.4 -237.8 -0.6 -237.1C65.1 -236.3 130.3 -234.6 185.7 -204.7"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(501.55704797434214 436.7009688101258)">
+          <path
+            id="blob-sprite-6"
+            d="M125.7 -163.1C161 -120.2 186.4 -78.8 197.5 -32.3C208.7 14.1 205.7 65.6 189.1 123.1C172.5 180.7 142.3 244.5 91.6 271.5C40.9 298.5 -30.3 288.8 -97.7 265.6C-165.1 242.3 -228.7 205.5 -266.8 149.3C-304.8 93.1 -317.4 17.5 -298.8 -46.5C-280.1 -110.5 -230.4 -162.9 -175.4 -202.4C-120.4 -241.8 -60.2 -268.4 -7.5 -259.5C45.2 -250.5 90.4 -206.1 125.7 -163.1"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(462.59476267180435 470.13097798231604)">
+          <path
+            id="blob-sprite-7"
+            d="M134.6 -157.8C175.8 -125.9 211.5 -84.7 235.9 -29.3C260.3 26.1 273.5 95.8 244.8 137.4C216.1 178.9 145.5 192.4 85.7 197.8C25.9 203.3 -23.1 200.7 -83.1 193.9C-143.1 187 -214 175.7 -251.2 134C-288.4 92.3 -291.9 20.1 -280.6 -50.4C-269.3 -120.9 -243.1 -189.7 -193.9 -220.2C-144.6 -250.7 -72.3 -242.8 -12.8 -227.6C46.7 -212.3 93.4 -189.7 134.6 -157.8"
+            fill="#EF2D56"
+          ></path>
+        </g>
+
+        <g transform="translate(427.10044625391487 429.2183231674454)">
+          <path
+            id="blob-sprite-8"
+            d="M144.9 -155.2C199.4 -126.9 263.2 -92.3 281.6 -41.7C300 8.9 273 75.4 237.6 137.5C202.2 199.6 158.4 257.2 102.8 273.5C47.3 289.7 -20 264.7 -77 234.1C-134.1 203.6 -180.9 167.5 -208.2 120.2C-235.6 72.9 -243.3 14.3 -241.5 -50.7C-239.7 -115.7 -228.3 -187.1 -186.5 -217.6C-144.6 -248 -72.3 -237.5 -13.6 -221.4C45.2 -205.2 90.4 -183.4 144.9 -155.2"
+            fill="#EF2D56"
+          ></path>
+        </g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      masterId: 'blob-sprite-1',
+    }
+  },
+  mounted() {
+    this.initBlob()
+    this.blobElm = document.getElementById(this.masterId)
+  },
+  methods: {
+    initBlob() {
+      const tween1 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-2' },
+        { duration: 3000, easing: 'easingCircularInOut' }
+      )
+
+      const tween2 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-3' },
+        { duration: 3500, easing: 'easingCircularInOut' }
+      )
+
+      const tween3 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-4' },
+        { duration: 1500, easing: 'easingCircularInOut' }
+      )
+
+      const tween4 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-5' },
+        { duration: 2000, easing: 'easingCircularInOut' }
+      )
+
+      const tween5 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-6' },
+        { duration: 1500, easing: 'easingCircularInOut' }
+      )
+
+      const tween6 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-7' },
+        { duration: 2500, easing: 'easingCircularInOut' }
+      )
+
+      const tween7 = this.$KUTE.to(
+        '#blob-sprite-1',
+        { path: '#blob-sprite-8' },
+        { duration: 1500, easing: 'easingCircularInOut' }
+      )
+
+      tween7.chain(tween1)
+      tween6.chain(tween7)
+      tween5.chain(tween6)
+      tween4.chain(tween5)
+      tween3.chain(tween4)
+      tween2.chain(tween3)
+      tween1.chain(tween2)
+
+      tween1.start()
+    },
+  },
+}
+</script>
+
+<style scoped>
+[data-section-hero] {
+  position: fixed;
+  z-index: 50;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none;
+  padding: 10;
+}
+
+[data-section-hero]::after {
+  content: '';
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.13;
+  background-image: url('/images/grain.gif');
+}
+
+path:not(#blob-sprite-1) {
+  visibility: hidden;
+}
+</style>
